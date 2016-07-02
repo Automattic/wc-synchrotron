bfa94508ec9c2518af4ec8c3ed0a5dd67f109ac2
Kevin Killingsworth <kevin@redfenix.com>
2016-07-02 16:29:51 -0500

    Add products screen and header
    This adds the products screen component and
    the screen header for it.

---
+++ client/app/products-screen/index.jsx
@@ -0,0 +1,52 @@
+import React, { PropTypes } from 'react';
+import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import TitleBar from '../../components/title-bar';
+import Button from 'components/button';
+
+class ProductsScreen extends React.Component {
+	propTypes: {
+		products: PropTypes.object.isRequired
+	}
+
+	constructor( props ) {
+		super( props );
+	}
+
+	render() {
+		const __ = this.props.translate;
+		const onEdit = null; // TODO: hook up to bound action creator
+		const onAdd = null; // TODO: hook up to bound action creator
+
+		return (
+			<div className="products__screen">
+				<div className="products__screen-header">
+					<TitleBar icon="product" title={ __( 'Products' ) }>
+						<Button onClick={ onEdit } >{ __( 'Edit products' ) }</Button>
+						<Button primary onClick={ onAdd } >{ __( 'Add product' ) }</Button>
+					</TitleBar>
+				</div>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps( state ) {
+	const { products } = state;
+
+	return {
+		// TODO: Add products to state.
+	};
+}
+
+function mapDispatchToProps( dispatch ) {
+	return bindActionCreators(
+		{
+			// TODO: Add actions
+		},
+		dispatch
+	);
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( localize( ProductsScreen ) );
