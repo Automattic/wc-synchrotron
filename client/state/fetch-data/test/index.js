f4324403b7c516a20d8ee1bf6f748e27c47c9b62
Kevin Killingsworth <kevin@redfenix.com>
2016-11-10 23:18:09 -0600

    fetch-data: Add chai tests for FetchConnect
    The FetchConnect HOC did not have any tests for it, so this
    commit adds some. They're not 100% comprehensive as we would
    have to load up JSDom or run these in the browser to accomplish
    that. But using `skin-deep`, it was possible to test almost all
    normal functionality.
    
    Note: Error tests are not included yet, as that functionality is
    still to be done.

--- client/state/fetch-data/test/index.js
+++ client/state/fetch-data/test/index.js
@@ -1,29 +1,141 @@
 import { expect } from 'chai';
+import React from 'react';
+import sd from 'skin-deep';
+import { createMockStore } from 'redux-test-utils';
 
-import { getData } from '../';
+import { fetchConnect, updateWhen } from '../';
+import reducer from '../reducer';
+
+// Constants used in the tests below.
+const service = 'my-service';
+const endpoint = 'my/endpoint';
+
+function fetchActionSuccess( service, key, data ) {
+	// Simulates a successful API fetch.
+	return {
+		type: 'WC_FETCH_DATA_FETCHED',
+		payload: { service, key, data }
+	};
+}
+
+function fetchActionError( service, key, error ) {
+	// Simulates a failed API fetch.
+	return {
+		type: 'WC_FETCH_DATA_ERROR',
+		payload: { service, key, error }
+	};
+}
+
+function createFetch( query, defaultValue, fetchValue ) {
+	const key = endpoint + query;
+
+	return {
+		service,
+		key,
+		defaultValue,
+		shouldUpdate: updateWhen.notPresent(),
+		action: ( state ) => {
+			return fetchActionSuccess( service, key, fetchValue );
+		}
+	}
+}
 
 describe( 'fetch-data', () => {
-	describe( '#getData()', () => {
-		it( 'should fetch stored data', () => {
-			const myData = { name: 'myData', value1: 1, value2: 2.2, value3: -3.33 };
-			const state = {
-				myService: {
-					myQuery: myData
+	describe( '#updateWhen', () => {
+
+		describe( '#notPresent()', () => {
+			const query = '?q=x';
+			const defaultValue = [];
+			const fetchValue = [ 1, 2, 3 ];
+
+			const fetch = createFetch( query, defaultValue, fetchValue );
+
+			it( 'should check for state data', () => {
+				expect( fetch.shouldUpdate( fetch, defaultValue ) ).to.be.true;
+				expect( fetch.shouldUpdate( fetch, fetchValue ) ).to.be.false;
+			} );
+		} );
+	} );
+
+	describe( '#fetchConnect()', () => {
+		const query1 = '?q=1';
+		const query2 = '?q=2';
+		const defaultValue = [];
+		const initialValue = [ 1, 2, 3 ];
+		const fetchValue = [ 1, 2.2, -3.33 ];
+
+		function testFetch( query ) {
+			return createFetch( query, defaultValue, fetchValue );
+		}
+
+		// Create a test component that uses the fetch.
+		const TestComponent = ( props ) => {
+			return (
+				<div>
+					<span>{ props.query }</span>
+					<span>{ props.fetchResult }</span>
+				</div>
+			);
+		};
+
+		// Map the queryValue property on the test component.
+		function mapFetchProps( props ) {
+			return {
+				fetchResult: testFetch( props.query )
+			};
+		}
+		const FetchComponent = fetchConnect( mapFetchProps )( TestComponent );
+
+		it( 'should map fetchResult to props', () => {
+			const initialState = {
+				fetchData: {
+					[ service ]: {
+						[ endpoint + query1 ]: initialValue,
+					}
 				}
 			};
 
-			const result = getData( 'myService', 'myQuery', state );
+			const store = createMockStore( initialState );
+			const props = { query: query1, store: store };
 
-			expect( result ).to.equal( myData );
+			// Render the test component
+			const tree = sd.shallowRender( React.createElement( FetchComponent, props ) );
+			tree.reRender( props );
+
+			expect( tree.props.query ).to.equal( query1 );
+			expect( tree.props.fetchResult ).to.equal( initialValue );
 		} );
 
-		it( 'should return null if query not found', () => {
-			const state = {
+		it( 'should fetch when the query changes', () => {
+			const initialState = {
+				fetchData: {
+					[ service ]: {
+						[ endpoint + query1 ]: initialValue,
+					}
+				}
 			};
 
-			const result = getData( 'myService', 'myQuery', state );
+			let store = createMockStore( initialState );
+			const props = { query: query1, store: store };
 
-			expect( result ).to.be.null;
+			// Render the test component
+			const tree = sd.shallowRender( React.createElement( FetchComponent, props ) );
+			tree.reRender( props );
+			tree.reRender( { query: query2, store } );
+
+			expect( store.getActions() ).to.eql( [ testFetch( query2 ).action( store.getState() ) ] );
+
+			// Regenerate the store with a real reduced state for testing the resulting fetchResult property.
+			initialState.fetchData = reducer( initialState.fetchData, store.getActions()[0] );
+			console.log( 'new state' );
+			console.log( JSON.stringify( store.getState() ) );
+
+			// fetchConnect subscribes to the store's updates, but the mock doesn't do that.
+			// so we have to update manually here.
+			tree.reRender( { query: query2, store } );
+
+			expect( tree.props.query ).to.equal( query2 );
+			expect( tree.props.fetchResult ).to.equal( fetchValue );
 		} );
 	} );
 } );
