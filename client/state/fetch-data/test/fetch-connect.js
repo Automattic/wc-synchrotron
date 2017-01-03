2891f6be2ea83887d43002aceb6c8fc8e8b5fb53
Kevin Killingsworth <kevin@redfenix.com>
2017-01-03 18:53:52 -0500

    fetch-data: Split fetch-connect into its own file.
    The fetchConnect HOC has gotten big enough to warrant its own file,
    and the index file needs to contain its own class as well, so this
    commit splits these out to two separate files.

---
+++ client/state/fetch-data/test/fetch-connect.js
@@ -0,0 +1,130 @@
+import { expect } from 'chai';
+import React from 'react';
+import sd from 'skin-deep';
+import { createMockStore } from 'redux-test-utils';
+
+import { updateWhen } from '../';
+import fetchConnect from '../fetch-connect';
+import reducer from '../reducer';
+
+// Constants used in the tests below.
+const service = 'my-service';
+const endpoint = 'my/endpoint';
+
+function fetchActionSuccess( service, key, data ) {
+	// Simulates a successful API fetch.
+	return {
+		type: 'WC_FETCH_DATA_FETCHED',
+		payload: { service, key, data }
+	};
+}
+
+function fetchActionError( service, key, error ) {
+	// Simulates a failed API fetch.
+	return {
+		type: 'WC_FETCH_DATA_ERROR',
+		payload: { service, key, error }
+	};
+}
+
+function createFetch( query, defaultValue, fetchValue ) {
+	const key = endpoint + query;
+
+	return {
+		service,
+		key,
+		defaultValue,
+		shouldUpdate: updateWhen.notFetched(),
+		action: ( state ) => {
+			return fetchActionSuccess( service, key, fetchValue );
+		}
+	}
+}
+
+describe( '#fetchConnect()', () => {
+	const query1 = '?q=1';
+	const query2 = '?q=2';
+	const defaultValue = [];
+	const initialValue = [ 1, 2, 3 ];
+	const fetchValue = [ 1, 2.2, -3.33 ];
+
+	function testFetch( query ) {
+		return createFetch( query, defaultValue, fetchValue );
+	}
+
+	// Create a test component that uses the fetch.
+	const TestComponent = ( props ) => {
+		return (
+			<div>
+				<span>{ props.query }</span>
+				<span>{ props.fetchResult }</span>
+			</div>
+		);
+	};
+
+	// Map the queryValue property on the test component.
+	function mapFetchProps( props ) {
+		return {
+			fetchResult: testFetch( props.query )
+		};
+	}
+	const FetchComponent = fetchConnect( mapFetchProps )( TestComponent );
+
+	it( 'should map fetchResult to props', () => {
+		const initialState = {
+			fetchData: {
+				[ service ]: {
+					[ endpoint + query1 ]: {
+						value: initialValue,
+					}
+				}
+			}
+		};
+
+		const store = createMockStore( initialState );
+		const props = { query: query1, store: store };
+
+		// Render the test component
+		const tree = sd.shallowRender( React.createElement( FetchComponent, props ) );
+		tree.reRender( props );
+
+		expect( tree.props.query ).to.equal( query1 );
+		expect( tree.props.fetchResult ).to.equal( initialValue );
+	} );
+
+	it( 'should fetch when the query changes', () => {
+		const initialState = {
+			fetchData: {
+				[ service ]: {
+					[ endpoint + query1 ]: {
+						value: initialValue,
+					}
+				}
+			}
+		};
+
+		let store = createMockStore( initialState );
+		const props = { query: query1, store: store };
+
+		// Render the test component
+		const tree = sd.shallowRender( React.createElement( FetchComponent, props ) );
+		tree.reRender( props );
+		tree.reRender( { query: query2, store } );
+
+		expect( store.getActions() ).to.eql( [
+			testFetch( query1 ).action( store.getState() ),
+			testFetch( query2 ).action( store.getState() ),
+		] );
+
+		// Regenerate the store with a real reduced state for testing the resulting fetchResult property.
+		initialState.fetchData = reducer( initialState.fetchData, store.getActions()[1] );
+
+		// fetchConnect subscribes to the store's updates, but the mock doesn't do that.
+		// so we have to update manually here.
+		tree.reRender( { query: query2, store } );
+
+		expect( tree.props.query ).to.equal( query2 );
+		expect( tree.props.fetchResult ).to.equal( fetchValue );
+	} );
+} );
+
