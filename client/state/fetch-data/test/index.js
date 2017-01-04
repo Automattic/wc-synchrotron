4359b67c9393f3e17cbd74902bab4caa9a7d2baa
Kevin Killingsworth <kevin@redfenix.com>
2017-01-03 21:12:29 -0500

    fetch-data: Add subscribe/unsubscribe tests.
    This adds unit tests for FetchData subscribe and unsubscribe.
    It also changes some console.log lines to proper logging

--- client/state/fetch-data/test/index.js
+++ client/state/fetch-data/test/index.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import { updateWhen } from '../';
+import { updateWhen, FetchData } from '../';
 
 // Constants used in the tests below.
 const service = 'my-service';
@@ -49,6 +49,97 @@ describe( 'fetch-data', () => {
 			it( 'should check for state data', () => {
 				expect( fetch.shouldUpdate( fetch, { data: defaultValue } ) ).to.be.true;
 				expect( fetch.shouldUpdate( fetch, { data: fetchValue, lastFetchTime: Date.now() } ) ).to.be.false;
+			} );
+		} );
+	} );
+
+	describe( 'FetchData', () => {
+		describe( '#subscribe() and #unsubscribe()', () => {
+			const fetch = createFetch( '?query', [], [ 4, 5, 6 ] );
+
+			it( 'should subscribe successfully.', () => {
+				const fetchData = new FetchData();
+				const subscribeCall = () => {
+					fetchData.subscribe( fetch );
+				}
+
+				expect( subscribeCall ).to.not.throw( Error );
+			} );
+
+			it( 'should return a handle.', () => {
+				const fetchData = new FetchData();
+				const handle = fetchData.subscribe( fetch );
+
+				expect( handle ).to.exist;
+				expect( handle ).to.be.an( 'object' );
+			} );
+
+			it( 'should provide redux state selectors.', () => {
+				const fetchData = new FetchData();
+				const handle = fetchData.subscribe( fetch );
+				const fetchValue = [ 'fetch', 'value' ];
+				const fetchStatus = { lastFetchTime: 123, lastSuccessTime: 124 };
+				const state = {
+					'fetchData': {
+						[ fetch.service ]: {
+							[ fetch.key ]: {
+								value: fetchValue,
+								status: fetchStatus,
+							}
+						}
+					}
+				};
+
+				expect( handle.value ).to.be.a( 'function' );
+				expect( handle.value( state ) ).to.equal( fetchValue );
+
+				expect( handle.status ).to.be.a( 'function' );
+				expect( handle.status( state ) ).to.equal( fetchStatus );
+			} );
+
+			it( 'should provide an unsubscribe function.', () => {
+				const fetchData = new FetchData();
+				const handle = fetchData.subscribe( fetch );
+				const unsubscribeCall = () => {
+					handle.unsubscribe()
+				};
+
+				expect( handle.unsubscribe ).to.be.a( 'function' );
+				expect( unsubscribeCall ).to.not.throw( Error );
+			} );
+
+			it( 'should allow a subscribe after an unsubscribe.', () => {
+				const fetchData = new FetchData();
+				const handle = fetchData.subscribe( fetch );
+				const subscribeCall = () => {
+					fetchData.subscribe( fetch );
+				};
+				const unsubscribeCall = () => {
+					handle.unsubscribe()
+				};
+
+				expect( handle.unsubscribe ).to.be.a( 'function' );
+				expect( unsubscribeCall ).to.not.throw( Error );
+				expect( subscribeCall ).to.not.throw( Error );
+			} );
+
+			it( 'should throw Error upon invalid unsubscribe', () => {
+				const fetchData = new FetchData();
+				const unsubscribeCall = () => {
+					fetchData.unsubscribe( fetch )
+				};
+
+				expect( unsubscribeCall ).to.throw( Error );
+			} );
+
+			it( 'should throw Error upon invalid subscribe', () => {
+				const fetchData = new FetchData();
+				const subscribeCall = () => {
+					fetchData.subscribe( fetch );
+				};
+
+				expect( subscribeCall ).to.not.throw( Error );
+				expect( subscribeCall ).to.throw( Error );
 			} );
 		} );
 	} );
